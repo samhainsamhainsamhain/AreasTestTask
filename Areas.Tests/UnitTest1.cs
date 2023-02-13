7a5d5dcbde40aa07dd2f2046abea44d36d8a1ab3
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-02-13 16:48:24 +0300

    Add project files.

---
+++ Areas.Tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+using static Areas.Areas;
+
+namespace Areas.Tests
+{
+    [TestClass]
+    public class AreasTests
+    {
+        [TestMethod]
+        public void RectangeAreaCalculationTest()
+        {
+            var rectangle = new Rectangle(5, 5);
+
+            var rectArea = rectangle.CalculateArea();
+
+            Assert.AreEqual(25, rectArea);
+        }
+        [TestMethod]
+        public void CircleAreaCalculationTest()
+        {
+            var circle = new Circle(3);
+
+            var circleArea = circle.CalculateArea();
+
+            Assert.AreEqual(28.274333882308138, circleArea);
+        }
+        [TestMethod]
+        public void TriangleAreaCalculationTest()
+        {
+            var triangle = new Triangle(5, 5, 5);
+
+            var triangleArea = triangle.CalculateArea();
+
+            Assert.AreEqual(10.825317547305483, triangleArea);
+        }
+        [TestMethod]
+        public void TestIsRightTriangle_True()
+        {
+            var triangle = new Triangle(5, 4, 3);
+
+            var triangleIsRight = triangle.IsRightAngledTriangle;
+
+            Assert.IsTrue(triangleIsRight);
+        }
+        [TestMethod]
+        public void TestIsRightTriangle_False()
+        {
+            var triangle = new Triangle(5, 6, 1);
+
+            var triangleIsRight = triangle.IsRightAngledTriangle;
+
+            Assert.IsFalse(triangleIsRight);
+        }
+    }
+}
---
+++ Areas/Areas.cs
@@ -0,0 +1,81 @@
+﻿namespace Areas
+{
+    public class Areas
+    {
+        public interface Areable
+        {
+            double CalculateArea();
+        }
+
+        public class Circle : Areable
+        {
+
+            public double Radius { get; }
+
+            public Circle(double radius)
+            {
+                if (radius < 0)
+                    throw new ArgumentOutOfRangeException("Radius must be more than 0");
+
+                Radius = radius;
+            }
+
+            public double CalculateArea()
+            {
+                return Radius * Radius * Math.PI;
+            }
+        }
+        public class Rectangle : Areable
+        {
+            public double Width { get; }
+            public double Height { get; }
+
+            public Rectangle(double width, double height)
+            {
+                if (width < 0 || height < 0)
+                    throw new ArgumentOutOfRangeException("Rectangle sides must be more than 0");
+
+                Width = width;
+                Height = height;
+            }
+
+            public double CalculateArea()
+            {
+                return Height * Width;
+            }
+        }
+        public class Triangle : Areable
+        {
+            private readonly Lazy<bool> _isRightAngled;
+            private double SideA { get; }
+            private double SideB { get; }
+            private double SideC { get; }
+            public bool IsRightAngledTriangle => _isRightAngled.Value;
+
+            public Triangle(double sideA, double sideB, double sideC)
+            {
+                if (sideA < 0 || sideB < 0 || sideC < 0)
+                    throw new ArgumentOutOfRangeException("Side length can't be negative");
+
+                SideA = sideA;
+                SideB = sideB;
+                SideC = sideC;
+
+                _isRightAngled = new Lazy<bool>(CheckIsRightAngled);
+            }
+
+            public double CalculateArea()
+            {
+                double p = (SideA + SideB + SideC) / 2;
+
+                return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+            }
+            private bool CheckIsRightAngled()
+            {
+                var maxSide = new[] { SideA, SideB, SideC }.Max();
+                var maxSideSqr = maxSide * maxSide;
+                return maxSideSqr + maxSideSqr == SideA * SideA + SideB * SideB + SideC * SideC;
+            }
+        }
+    }
+}
